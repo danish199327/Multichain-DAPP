2bdc3d90a8fd255d29add0807b7c2d1415b79164
Muhammad Danish Zaki <125446507+danish199327@users.noreply.github.com>
2024-07-15 23:23:27 +0500

    Update multichainConfig.js

--- multichainConfig.js
+++ multichainConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    host: '192.168.10.6', // Replace with your Multichain node IP
-    port: 7358,        // Replace with your Multichain node RPC port
-    user: 'multichainrpc', // Replace with your Multichain node RPC user
-    pass: 'FwTzS6wFHUtJNcNTwUkSTNC5tkLkeWNToG4vMXvSoyJj',  // Replace with your Multichain node RPC password
+    host: 'xxx', // Replace with your Multichain node IP
+    port: xxx,        // Replace with your Multichain node RPC port
+    user: 'xxxx', // Replace with your Multichain node RPC user
+    pass: 'xxxx',  // Replace with your Multichain node RPC password
 };
